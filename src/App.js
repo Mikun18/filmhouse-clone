3c583c712ef4c6582be98d6d213755a3b2b9cfb0
mikun <mikun@gmail.com>
2022-06-13 16:05:51 -0800

    Used hashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './homepage';
 import Subscribe from './subscribe';
 import Navbar from './navbar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path='/' element={<Navbar/>}>
           <Route path='subscribe' element={<Subscribe />}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
