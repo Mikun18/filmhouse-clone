523e5db6d74a02fa418ab49009be917a43fd9eaf
mikun <mikun@gmail.com>
2022-06-16 07:48:00 -0800

    fixed routing

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Bookings from './bookings';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path='/filmhouse-clone' element={<Homepage />} />
-        <Route path='/filmhouse-clone' element={<Navbar/>}>
-          <Route path='subscribe' element={<Subscribe />}/>
-          <Route path='food' element={<Food />} />
-          <Route path='bookings' element={<Bookings />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/' element={<Navbar/>}>
+            <Route path='subscribe' element={<Subscribe />}/>
+            <Route path='food' element={<Food />} />
+            <Route path='bookings' element={<Bookings />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
